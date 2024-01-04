84a78b9566f091c30a3984c0627c3d9df29908e9
thhvvv <makausilvia964@gmail.com>
2024-01-04 12:28:32 -0800

    infinite add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-if __name == "__main__":
-    import sys
-    arguments = sys.argv[1:]
-    result = 0
-    if len(arguments) == 0:
-        print("{}".format(result))
-    else:
-        for argument in arguments:
-            result += int(argument)
-            print(result)
+if __name__ == "__main__":
+    from sys import argv
+    sum = 0
+    for n in range(1, len(argv)):
+        sum += int(argv[n])
+    print(sum)
