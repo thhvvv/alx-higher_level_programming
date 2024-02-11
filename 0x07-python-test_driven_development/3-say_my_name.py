8d08ed8c296bd8f04eee0c54bd14e8d54ea7fc8f
thhvvv <makausilvia964@gmail.com>
2024-02-11 07:10:33 -0800

    task2

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-"""prints a name"""
+""" prints a name """
+
 
 def say_my_name(first_name, last_name=""):
-    """prints a name
-    args:
-    first_name(str) - firstname to print
-    last_name(str) - last name to print
+    """Print a name.
+
+    Args:
+        first_name (str): The first name to print.
+        last_name (str): The last name to print.
+    Raises:
+        TypeError: If either of first_name or last_name are not strings.
     """
 
     if not isinstance(first_name, str):
@@ -13,4 +17,4 @@ def say_my_name(first_name, last_name=""):
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
-    print("My name is {} {} ".format(first_name, last_name))
+    print("My name is {} {}".format(first_name, last_name))
