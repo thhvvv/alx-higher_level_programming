1f61983181c84e2cfaff37b34344269ced8eaf39
thhvvv <makausilvia964@gmail.com>
2024-04-10 01:55:35 -0700

    3-5

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-y = abs(number) % 10
 if number < 0:
-    y = -y
-    if y > 5:
-        print("Last digit of {} is {} and is greater than 5".format(number, y))
-    elif y == 0:
-        print("Last digit of {} is {} and is 0".format(number, y))
-    elif x < 6 and x != 0:
-        print("Last digit of {} is {} and is less than 6 and not 0".format(number, x))
+    lastdigit = number % -10
+else:
+    lastdigit = number % 10
+if lastdigit > 5:
+    print("Last digit of {:d} is {:d} and is greater than 5"
+          .format(number, lastdigit))
+elif lastdigit < 6 and lastdigit != 0:
+    print("Last digit of {:d} is {:d} and is less than 6 and not 0"
+          .format(number, lastdigit))
+else:
+    print("Last digit of {:d} is 0 and is 0".format(number))
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for numbers in range(0, 100):
-    if numbers == 99:
-        print("{}".format(numbers))
-    else:
-        print("{:02d}".format(numbers), end=", ")
+for num in range(0, 99):
+    print('{:02d}, '.format(num), end='')
+print('99')
